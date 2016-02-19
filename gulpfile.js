734a57c71c8911c2f78c51435615ee5dd6094733
hrandp <danniel@hrandp.com>
2016-02-19 01:06:07 -0800

    Added gulp mustache plugin

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
 	concat = require('gulp-concat'),
+	browserify = require('gulp-browserify'),
 	compass = require('gulp-compass');
 
 //Variable Declorations
@@ -45,5 +46,6 @@ gulp.task('compass', function(){
 gulp.task('js', function(){
 	gulp.src(jsSources)
 		.pipe(concat('main.js'))
+		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
 });
